05feb29f34c8749aa718dc21b1cca42e9a674403
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-14 23:08:52 -0300

    add tab and newline

--- part_1/002_strings.py
+++ part_1/002_strings.py
@@ -9,3 +9,9 @@ first_name = 'marcos'
 last_name = 'boscariol'
 full_name = f'{first_name} {last_name}'
 print(f'Hello, {full_name.title()}!')
+
+# Add a tab
+print('\tPython')
+
+# Add newline
+print('Languages:\nPython\nC\nJava')
