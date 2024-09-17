7c86ab4d452cb40a902436373ce40e86095094de
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-17 13:03:51 -0300

    modyfying elements

--- part_1/004_lists.py
+++ part_1/004_lists.py
@@ -9,3 +9,9 @@ print(bycicles[-1])
 # Using Individual Values from a List
 message = f'My first bycicle was a {bycicles[2].title()}.'
 print(message)
+
+# Modifying Elements in a List
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
