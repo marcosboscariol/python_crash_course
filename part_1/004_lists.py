42348547ae15148e5cdbef3b892f384cf6b21be7
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-17 13:24:14 -0300

    removing elements

--- part_1/004_lists.py
+++ part_1/004_lists.py
@@ -32,3 +32,32 @@ print(motorcycles)
 motorcycles = ['honda', 'yamaha', 'suzuki']
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
+
+# Removing Elements from a List
+
+# del Statement
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[0]
+print(motorcycles)
+
+# pop() Method
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motorcycles.pop()
+print(f'The last motorcycle that I bought was a {last_owned.title()}.')
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print(f'The first motorcycle that I bpught was a {first_owned.title()} ')
+
+# Removng an Item by Value
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
