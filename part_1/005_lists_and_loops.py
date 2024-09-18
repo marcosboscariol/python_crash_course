ef31cffc5397945be50010c10db1b68ddc7982a8
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-18 12:05:29 -0300

    final print message

--- part_1/005_lists_and_loops.py
+++ part_1/005_lists_and_loops.py
@@ -9,3 +9,5 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(f'You did a great job, {magician.title()}!')
     print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+print('Thank you everyone, that was a great magic show!')
