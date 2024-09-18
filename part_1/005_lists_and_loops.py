d2d7c47ae4be7a74166542a5db2dcfa00b7d0bd1
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-18 11:50:21 -0300

    impoving the print message

--- part_1/005_lists_and_loops.py
+++ part_1/005_lists_and_loops.py
@@ -4,3 +4,8 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f'You did a great job, {magician.title()}!')
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
