9eaa071e138faae192be5a2b79dbf3d9c783b773
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-20 17:57:46 -0300

    statistics

--- part_1/005_lists_and_loops.py
+++ part_1/005_lists_and_loops.py
@@ -11,3 +11,24 @@ for magician in magicians:
     print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
 print('Thank you everyone, that was a great magic show!')
+
+# Making Numerical Lists
+
+# Using range() Function
+for value in range(1, 10):
+    print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+squares = []
+for number in range(1, 11):
+    squares.append(number**2)
+print(squares)
+
+# Simple Statistics with a List of Numbers
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+print(max(digits))
+print(min(digits))
+print(sum(digits))
