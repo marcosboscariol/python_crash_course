d960bb394f695a3b43c60f18e1be0cad4aaf779b
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-23 12:44:49 -0300

    list comprehension

--- part_1/005_lists_and_loops.py
+++ part_1/005_lists_and_loops.py
@@ -32,3 +32,7 @@ digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(max(digits))
 print(min(digits))
 print(sum(digits))
+
+# List comprehensions
+squares = [value ** 2 for value in range(1, 11)]
+print(squares)
