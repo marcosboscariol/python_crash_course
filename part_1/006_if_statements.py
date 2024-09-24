2e60703d3c73f8b0f2e979d29da3175d01e5433c
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-24 13:25:41 -0300

    simple if statement

--- part_1/006_if_statements.py
+++ part_1/006_if_statements.py
@@ -45,9 +45,14 @@ age_1 = 18
 print(age_0 >= 21 or age_1 >= 21)
 
 # Checking Whether a Value us Not in a List
-
 banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 
 if user not in banned_users:
     print(f'{user.title()}, you can post.')
+
+# Simple if Statement
+age = 19
+
+if age >= 18:
+    print('You are old enough to vote.')
