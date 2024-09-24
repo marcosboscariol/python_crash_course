3fd0a64d1bdc210eb3115b458f40081d7286baf4
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-24 13:20:17 -0300

    value in list

--- part_1/006_if_statements.py
+++ part_1/006_if_statements.py
@@ -43,3 +43,11 @@ age_0 = 22
 age_1 = 18
 
 print(age_0 >= 21 or age_1 >= 21)
+
+# Checking Whether a Value us Not in a List
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f'{user.title()}, you can post.')
