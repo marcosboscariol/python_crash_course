c6e9d52450ece62166a3e405c4bc5eb09a1e69cf
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-24 13:08:32 -0300

    conditional tests

--- part_1/006_if_statements.py
+++ part_1/006_if_statements.py
@@ -7,3 +7,25 @@ for car in cars:
         print(car.upper())
     else:
         print(car.title())
+
+# Conditional Tests
+
+# Checking for Equality
+car = 'bmw'
+print(car == 'bmw')
+
+car = 'audi'
+print(car == 'bmw')
+
+# Checking for Inequality
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+# Numerical Comparison
+age = 18
+
+print(age == 18)
+print(age < 18)
+print(age > 18)
