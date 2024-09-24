1115bfa119d204b35344dbe1f0b95b8ff2002202
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-24 13:18:28 -0300

    multiple conditions

--- part_1/006_if_statements.py
+++ part_1/006_if_statements.py
@@ -29,3 +29,17 @@ age = 18
 print(age == 18)
 print(age < 18)
 print(age > 18)
+
+# Checking Multiple Conditions
+
+# Using and to Check Multipla Conditions
+age_0 = 22
+age_1 = 18
+
+print(age_0 >= 21 and age_1 >= 21)
+
+# Using or to Check Multipla Conditions
+age_0 = 22
+age_1 = 18
+
+print(age_0 >= 21 or age_1 >= 21)
