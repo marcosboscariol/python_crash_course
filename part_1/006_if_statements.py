19b07887f8df288700971555e2eb765636801a68
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2024-09-24 13:32:00 -0300

    if-elif-else chain

--- part_1/006_if_statements.py
+++ part_1/006_if_statements.py
@@ -73,3 +73,16 @@ if age >= 18:
 else:
     print("Sorry, you are too young to vote.")
     print("Please register to vote as soon as you turn 18!")
+
+
+# The if-elif-else Chain
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
+
+
+#
