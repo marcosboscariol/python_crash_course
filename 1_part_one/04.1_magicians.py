3b7b89dca6d3285023649b5f9b7255b05ec61e64
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2025-01-13 15:44:59 -0300

    add file

--- 1_part_one/04.1_magicians.py
+++ 1_part_one/04.1_magicians.py
@@ -1,1 +1,6 @@
-# page 87
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f'You did a great job {magician.title()}!')
+    print(f"{magician.title()}, I can't wait to see your next trick\n")
+print("Thank you, everyone. That was a great magic show!")
