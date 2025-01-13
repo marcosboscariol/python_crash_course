550aee0521e155ba03a87b711321b5b65439b88b
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2025-01-13 16:47:47 -0300

    add file

---
+++ 1_part_one/04.8_dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+print("Original Dimension:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (100, 500)
+print("New Dimension:")
+for dimension in dimensions:
+    print(dimension)
