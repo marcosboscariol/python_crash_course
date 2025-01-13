557f33c7a9ed652dca593862152f85a3c9d3a29e
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2025-01-12 23:07:54 -0300

    add file

---
+++ 1_part_one/03.3_cars.py
@@ -0,0 +1,20 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars))
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+print(len(cars))
